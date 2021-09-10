cc7a790abaf638d8cfc00f1e5d290df2531a44f9
Gleycy Souza <gleycy.ksouza@gmail.com>
2021-09-10 09:25:14 -0300

    Completei o script incluirPet (Post)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -7,7 +7,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 public class Pet {
 
@@ -32,6 +34,8 @@ public class Pet {
                 .statusCode(200)
                 .body("name", is("Spike"))
                 .body("status", is("available"))
+                .body("category.name", is("dog"))
+                .body("tags.name", contains("sta"))
         ;
     }
 }
