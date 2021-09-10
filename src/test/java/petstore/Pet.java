e8b485eb65749ec1fa176def4f0647dd108cb5d3
Gleycy Souza <gleycy.ksouza@gmail.com>
2021-09-10 09:14:38 -0300

    Incluídas assertivas para o body do teste incluirPet.

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 public class Pet {
 
@@ -28,6 +29,9 @@ public class Pet {
                 .post(uri)
                 .then()
                 .log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body("name", is("Spike"))
+                .body("status", is("available"))
+        ;
     }
 }
